718c2a65b566bb8a2b87e97d8d90325e9b8941b0
emotoyuusuke0824 <h0i8e2n4n@yahoo.co.jp>
2020-05-16 11:01:52 +0900

    Hello World 実装

---
+++ src/main/java/com/example/demo/trySpring/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.demo.trySpring;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+ 
+@Controller
+public class HelloController {
+	@GetMapping("/hello")
+	public String getHello() {
+		return "hello";
+	}
+
+}
