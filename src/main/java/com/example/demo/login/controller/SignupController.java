44ee283a7da970fef7b17cb5886074724a92ebb0
yusuke-emoto <emoyuu0824@gmail.com>
2020-06-06 16:24:46 +0900

    六章エラーメッセージの編集、実装

--- src/main/java/com/example/demo/login/controller/SignupController.java
+++ src/main/java/com/example/demo/login/controller/SignupController.java
@@ -5,8 +5,14 @@ import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.login.domain.model.GroupOrder;
+import com.example.demo.login.domain.model.SignupForm;
 
 @Controller
 public class SignupController {
@@ -18,13 +24,17 @@ public class SignupController {
 		return radio;
 	}
 	@GetMapping("/signup")
-	public String getSignUp(Model model) {
+	public String getSignUp(@ModelAttribute SignupForm form, Model model) {
 		radioMarriage=initRadioMarrige();
 		model.addAttribute("radioMarriage",radioMarriage);
 		return "login/signup";
 	}
 	@PostMapping("/signup")
-	public String postSignUp(Model model) {
+	public String postSignUp(@ModelAttribute @Validated(GroupOrder.class) SignupForm form, BindingResult bindingResult, Model model) {
+		if(bindingResult.hasErrors()) {
+			return getSignUp(form,model);
+		}
+		System.out.println(form);
 		return "redirect:/login";
 	}
 }
