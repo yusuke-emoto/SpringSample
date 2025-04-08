3f7d17721fa4641eb27b8c2490cfde298acffa41
江元勇介 <65233450+yusuke-emoto@users.noreply.github.com>
2025-04-08 21:11:18 +0900

    test2

--- src/main/java/com/example/demo/login/controller/HomeController.java
+++ src/main/java/com/example/demo/login/controller/HomeController.java
@@ -1,9 +1,14 @@
 package com.example.demo.login.controller;
+import java.io.IOException;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,11 +19,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import com.example.demo.login.domain.model.SignupForm;
 import com.example.demo.login.domain.model.User;
 import com.example.demo.login.domain.service.UserService;
-import java.io.IOException;
-import org.springframework.dao.DataAccessException;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 
 
 @Controller
@@ -60,6 +60,7 @@ public class HomeController {
 			form.setAge(user.getAge());
 			form.setMarriage(user.isMarriage());
 			model.addAttribute("signupForm",form);
+			// test
 		}
 		return "login/homeLayout";
 	}
